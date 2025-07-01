702ac8ece88405b3c9e9f06b9cdd5da020889a95
sameerraza7760 <sameerraza7760@gmail.com>
2025-07-01 16:18:48 +0500

    update

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
 import Todo from "@/models/todo.models";
+import dbConnect from "@/lib/dbConnect";
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     await dbConnect();
 
     const deletedTodo = await Todo.findByIdAndDelete(params.id);
-
     if (!deletedTodo) {
       return NextResponse.json({ error: "Todo not found" }, { status: 404 });
     }
@@ -22,15 +21,18 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     await dbConnect();
-
     const body = await req.json();
     const { title } = body;
 
     if (!title) {
-      return NextResponse.json({ error: "Text is required" }, { status: 400 });
+      return NextResponse.json({ error: "Title is required" }, { status: 400 });
     }
 
-    const updatedTodo = await Todo.findByIdAndUpdate(params.id, { title }, { new: true });
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      params.id,
+      { title },
+      { new: true }
+    );
 
     if (!updatedTodo) {
       return NextResponse.json({ error: "Todo not found" }, { status: 404 });
@@ -39,6 +41,6 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
     return NextResponse.json(updatedTodo);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Update failed" }, { status: 500 });
+    return NextResponse.json({ error: "Failed to update" }, { status: 500 });
   }
 }
