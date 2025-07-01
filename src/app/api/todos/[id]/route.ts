78bf1743d2d23ee8b9b3b48b08eda7f1a9a8e1c3
sameerraza7760 <sameerraza7760@gmail.com>
2025-07-01 13:50:57 +0500

    update

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,48 +1,50 @@
-
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Todo from "@/models/todo.models";
 import dbConnect from "@/lib/dbConnect";
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        await dbConnect();
 
-        const deletedTodo = await Todo.findByIdAndDelete(params.id);
+type Context = {
+  params: {
+    id: string;
+  };
+};
 
-        if (!deletedTodo) {
-            return NextResponse.json({ error: "Todo not found" }, { status: 404 });
-        }
+export async function DELETE(req: NextRequest, { params }: Context) {
+  try {
+    await dbConnect();
 
-        return NextResponse.json({ message: "Todo deleted successfully" });
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+    const deletedTodo = await Todo.findByIdAndDelete(params.id);
+
+    if (!deletedTodo) {
+      return NextResponse.json({ error: "Todo not found" }, { status: 404 });
     }
+
+    return NextResponse.json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+  }
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        await dbConnect();
+export async function PUT(req: NextRequest, { params }: Context) {
+  try {
+    await dbConnect();
 
-        const body = await req.json();
-        const { title } = body;
+    const body = await req.json();
+    const { title } = body;
 
-        if (!title) {
-            return NextResponse.json({ error: "Text is required" }, { status: 400 });
-        }
-
-        const updatedTodo = await Todo.findByIdAndUpdate(
-            params.id,
-            { title },
-            { new: true }
-        );
-
-        if (!updatedTodo) {
-            return NextResponse.json({ error: "Todo not found" }, { status: 404 });
-        }
-
-        return NextResponse.json(updatedTodo);
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "Update failed" }, { status: 500 });
+    if (!title) {
+      return NextResponse.json({ error: "Text is required" }, { status: 400 });
     }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(params.id, { title }, { new: true });
+
+    if (!updatedTodo) {
+      return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Update failed" }, { status: 500 });
+  }
 }
