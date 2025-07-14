73ac08880fa8b6e22670831dc3addc2e5f58ece6
sameerraza7760 <sameerraza7760@gmail.com>
2025-07-14 15:31:48 +0500

    add duplicate 'usman' and 'hammad' entries to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import TodoApp from "./todo/page";
 export default function Home() {
   return (
     <>
-    <h1>sameer</h1>
-    <h1>uzairi</h1>
-    <h1>ahsan</h1>
-    <h1>usman</h1>
-    <h1>usman bukhtar</h1>
-    <h1>hammad</h1>
-    <h1>usman</h1>
-    <TodoApp/>
+      <h1>sameer</h1>
+      <h1>uzairi</h1>
+      <h1>ahsan</h1>X
+      <h1>usman</h1>
+      <h1>usman bukhtar</h1>
+      <h1>hammad</h1>
+      <h1>usman</h1>
+        <h1>hammad</h1>
+      <TodoApp />
     </>
   );
 }
