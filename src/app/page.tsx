2c4787f367c6b09fb312b152f3b7a0ac4d82500f
sameerraza7760 <sameerraza7760@gmail.com>
2025-07-14 16:40:39 +0500

    remove redundant entries from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,7 @@ import TodoApp from "./todo/page";
 export default function Home() {
   return (
     <>
-      <h1>sameer</h1>
-      <h1>uzairi</h1>
-      <h1>ahsan</h1>X
-      <h1>usman</h1>
-      <h1>usman bukhtar</h1>
-      <h1>hammad</h1>
-      <h1>usman</h1>
-      <h1>hammad</h1>
-      <h1>usman here</h1>
-      <h1>zohan here</h1>
-      <h1>userman here</h1>
-      <h1>maaz here</h1>
-      <h1>maaz bhaau</h1>
+     
       <TodoApp />
     </>
   );
